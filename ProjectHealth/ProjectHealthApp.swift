ff93dfef2e1c4e70e3530fab2cc4d9cc10be1dd2
Aaron Huxley <aaronhuxley07@gmail.com>
2026-02-21 21:15:43 +0000

    Initial Commit

---
+++ ProjectHealth/ProjectHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectHealthApp.swift
+//  ProjectHealth
+//
+//  Created by Aaron Huxley on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProjectHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
